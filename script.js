acb6fdfe7379b0f45c2dd218244db8ecfdbe8a00
Olulanke-Mainasara <95680556+Olulanke-Mainasara@users.noreply.github.com>
2022-11-02 19:50:45 +0100

    About to make some changes

--- script.js
+++ script.js
@@ -42,56 +42,20 @@ function closemenu() {
 
 
 
-/*---------------Script for the image slider---------------*/
+/*---------------Script for the header images slider---------------*/
 
-responsiveSlider()
+new Glide(".heroGlide", {
+    type: "carousel",
+    perView: 1,
+}).mount();
 
-function responsiveSlider() {
 
-    var slider = document.getElementById("slider");
-    var sliderWidth = slider.offsetWidth;
-    var slideList = document.getElementById("slideWrap");
-    var count = 1;
-    var items = slideList.querySelectorAll("li").length;
-    var prev = document.getElementById("prev");
-    var next = document.getElementById("next");
 
-    window.addEventListener('resize', function() {
-        sliderWidth = slider.offsetWidth;
-    });
+/*---------------Script for the reviews slider---------------*/
 
-    var prevSlide = function() {
-        if(count > 1) {
-            count = count - 2;
-            slideList.style.left = "-" + count * sliderWidth + "px";
-            count++;
-        }
-
-        else if(count = 1) {
-            count = items - 1;
-            slideList.style.left = "-" + count * sliderWidth + "px";
-            count++;
-        }
-    };
-
-    var nextSlide = function() {
-        if(count < items) {
-            slideList.style.left = "-" + count * sliderWidth + "px";
-            count++;
-        }
-        else if(count = items) {
-            slideList.style.left = "0px";
-            count = 1;
-        }
-    };
-
-    next.addEventListener("click", function() {
-        nextSlide();
-    });
-
-    prev.addEventListener("click", function() {
-        prevSlide();
-    });
-
-    setInterval(nextSlide, 8000);
-}
+new Glide(".glide", {
+    type: "carousel",
+    autoplay: 5000,
+    perView: 4,
+    focusAt: 'center'
+}).mount();
